7a6e9ec6b3ea55248b8a4cb821c0a29cf738e668
finaxar <samiran.mudgalkar@finaxar..com>
2021-11-06 18:37:16 +0530

    Added the changes for data source loading

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import datetime
 from tools import Processor
 import boto3
 import uuid
+import json
 
 app = Flask(__name__)
 
@@ -14,17 +15,16 @@ def hello_world():
 
 @app.route("/create_atm", methods=["POST"])
 def create_atm():
-    pass
+    return jsonify(request.json)
 
 
-@app.route("/get_atm/<city_name>", methods=["GET"])
+@app.route("/get_atm", methods=["GET"])
 def get_atm():
     processor = Processor()
     city_name = request.args.get("city_name")
+    print(city_name)
     atm_list = processor.get_atm_list(city_name)
-    return {
-        "atm_list": atm_list
-    }
+    return jsonify({"atm": atm_list})
 
 
 if __name__ == "__main__":
